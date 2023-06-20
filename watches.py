650fd97751c93ff8f6cfbd816950ee40a2dfe2fa
DelZeiT <maksimcvetkov1106@gmail.com>
2023-06-20 15:13:59 +0300

    Добавил виджет Label в центр окна.

--- watches.py
+++ watches.py
@@ -5,9 +5,8 @@ window = Tk()
 window.title('Часы')
 window.geometry('300x200')
 
+# Label
+lbl_time = Label(window, font='Arial 20')
+lbl_time.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 window.mainloop()
-
-
-
-
