0b1fbd98dc9d2143d806e7319bbb1c4bb923be5c
DelZeiT <maksimcvetkov1106@gmail.com>
2023-06-27 13:41:02 +0300

    Добавил будильник.

--- watches.py
+++ watches.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from datetime import *
+from tkinter import messagebox
 import winsound
 
 today = date.today()
@@ -7,13 +8,78 @@ today = date.today()
 # окно
 window = Tk()
 window.title('Часы')
-window.geometry('500x400')
+window.geometry('500x300')
 window.configure(bg='#333333')
 
 # текстовое поле для ввода времени, на которое нужно установить будильник
+hour_input = Entry(window, width=13)
+hour_input.place(relx=0.2, rely=0.8)
 
-alarm_input = Entry(window, width=13)
-alarm_input.place(relx=0.4, rely=0.8)
+minute_input = Entry(window, width=13)
+minute_input.place(relx=0.4, rely=0.8)
+
+sec_input = Entry(window, width=13)
+sec_input.place(relx=0.6, rely=0.8)
+
+# глобальная переменная для хранения состояния гудка
+is_alarm_sounding = False
+
+
+# функция установки будильника
+def set_alarm():
+    now = datetime.now()
+
+    # получаем данные с поля ввода
+    hour_alarm = int(hour_input.get())
+    minute_alarm = int(minute_input.get())
+    sec_alarm = int(sec_input.get())
+
+    # проверяем заполнение всех полей ввода
+    if hour_alarm != '' and minute_alarm != '' and sec_alarm != '':
+        hour_alarm = int(hour_alarm)
+        minute_alarm = int(minute_alarm)
+        sec_alarm = int(sec_alarm)
+
+        # создаем объект datetime для будильника
+        alarm_time = datetime(now.year, now.month, now.day, hour_alarm, minute_alarm, sec_alarm)
+
+        # вычисляем разницу во времени между текущим временем и будильником
+        time_difference = alarm_time - now
+
+        # переводим разницу времени в миллисекунды
+        time_difference_ms = time_difference.total_seconds() * 1000
+
+        # устанавливаем таймер
+        window.after(int(time_difference_ms), alarm)
+
+        # стираем введенный текст с поля ввода
+        hour_input.delete(0, END)
+        minute_input.delete(0, END)
+        sec_input.delete(0, END)
+    else:
+        messagebox.showwarning('Ошибка', 'Введите время будильника')
+    # стираем введенный текст с поля ввода
+    hour_input.delete(0, END)
+    minute_input.delete(0, END)
+    sec_input.delete(0, END)
+
+
+# кнопка "Установить будильник"
+btn = Button(window, text='Установить будильник', command=set_alarm)
+btn.place(relx=0.35, rely=0.9)
+
+
+def alarm():
+    global is_alarm_sounding
+
+    # проверяем, если гудок уже воспроизводится, то не запускаем его повторно
+    if is_alarm_sounding:
+        return
+
+    is_alarm_sounding = True
+    winsound.Beep(250, 10000)
+    messagebox.showinfo('Будильник', 'Время истекло!')
+    is_alarm_sounding = False
 
 
 
@@ -34,42 +100,6 @@ def tick():
 
     if now.second == 0:  # если наступила новая минута
         winsound.Beep(190, 400)
-
-
-# функция установки будильника
-def set_alarm():
-    while True:
-        now = datetime.now()
-
-        alarm_str = alarm_input.get()
-
-        hours, minutes, seconds = alarm_str[0:2], alarm_str[3:5], alarm_str[6:8]
-
-        # Преобразовать часы, минуты и секунды в числа
-        alarm_hours = int(hours)
-        alarm_minutes = int(minutes)
-        alarm_seconds = int(seconds)
-
-        alarm_time = datetime.now().replace(hour=alarm_hours, minute=alarm_minutes, second=alarm_seconds, microsecond=0)
-
-        # таймер
-        time_difference = alarm_time - now
-
-        window.after(int(time_difference.total_seconds()) * 1000, set_alarm)
-
-        # стираем введенный текст с поля ввода
-        alarm_input.delete(0, END)
-
-
-# кнопка "Установить будильник"
-btn = Button(window, text='Установить будильник', command=set_alarm)
-btn.place(relx=0.35, rely=0.9)
-
-
-# функция вызова будильника
-def alarm():
-    winsound.Beep(250, 5000)
-
 
 # виджет часов
 lbl_hour_int = Label(window, font='Arial 50', bg="#00CC99", fg="#FFFFFF")
@@ -98,6 +128,9 @@ lbl_second_str.place(relx=0.6, rely=0.6)
 # виджет даты
 lbl_date = Label(window, text=today, font='Arial 10', background="#00CC33", foreground="#FFFFFF")
 lbl_date.grid(column=0, row=0)
+
+lbl_set_alarm = Label(window, text='Введите время будильника в формате HH, MM и SS')
+lbl_set_alarm.place(relx=0.2, rely=0.7)
 
 # запуск функции
 tick()
